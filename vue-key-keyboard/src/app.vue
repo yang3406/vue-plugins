2ae67797d4d507b1cdc4d26504b9ba3e72a93d97
yang3406 <1158890486@qq.com>
2019-03-05 17:47:11 +0800

    change sm

---
+++ vue-key-keyboard/src/app.vue
@@ -0,0 +1,25 @@
+<!--  -->
+<template>
+  <div>
+     <button @click="payshow()">点击支付</button>
+  </div>
+</template>
+
+<script>
+import vuePayKeyboard from './lib/vue-pay-keyboard';
+export default {
+  data() {
+    return {};
+  },
+
+  components: {},
+
+  computed: {},
+
+  mounted: {},
+
+  methods: {},
+};
+</script>
+<style lang='scss' scoped>
+</style>
